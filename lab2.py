f5224a4e2d886081e459387955210e0c683e754a
Alinasss1 <aseytkamalova@inbox.ru>
2023-02-04 14:00:07 +0600

    first

---
+++ lab2.py
@@ -0,0 +1,18 @@
+print ("Name:")
+name = input ()
+print ("Firstname:" )
+Firstname = input()
+print ("age:")
+age = int(input())
+
+if  age <= 17 :
+    print ("1 course")
+elif  17 < age <= 18:
+    print ("2 course")
+elif  18 < age <= 19:
+    print ("3 course") 
+elif  19 < age <= 20:
+    print ("4 course") 
+else: 
+    print ("not student")
+    
